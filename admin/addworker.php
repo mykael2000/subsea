a46e39106f0f3e2b5046ab84ae9298461b49b3d6
Michael <eramehmichael2000@gmail.com>
2025-05-25 13:26:49 +0100

    update

--- admin/addworker.php
+++ admin/addworker.php
@@ -4,9 +4,9 @@ include("includes/header.php");
 
 $message = "";
 if(isset($_POST['create'])){
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
     $uploadDir = "../profile/";
     $fileName = basename($_FILES["profile"]["name"]);
